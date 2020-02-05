19f62035995c0663ffa0fd60c695e7ebeda5bad2
Chaanchess <kira11fran@gmail.com>
2020-02-05 17:52:01 +0100

    Paginacion

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,21 +5,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Category;
+use App\Entity\Job;
+use App\Repository\JobRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 class CategoryController extends AbstractController{
+
+
     /**
      * Finds and displays a category entity
      * 
-     * @Route("/category/{slug}", name="category.show", methods="GET")
+     * @Route("/category/{slug}/{page}", name="category.show", methods="GET", defaults={"page":1}, requirements={"page" = "\d+"})
      * 
      * @param Category $category
+     * @param PaginatorInterface $paginator
+     * @param int $page
      * 
      * @return Response
      */
-    public function show(Category $category) : Response{
+    public function show(Category $category, int $page, PaginatorInterface $paginator) : Response{
+
+        $activeJobs = $paginator->paginate(
+            $this->getDoctrine()
+            ->getRepository(Job::class)
+            ->getPaginatedActiveJobsByCategoryQuery($category),
+            $page,
+            $this-> getParameter('max_jobs_on_category')
+        );
+
         return $this->render('category/show.html.twig', [
-            'category' => $category,
-        ]);
+            'category'=>$category, 
+            'activeJobs' => $activeJobs]
+        );
     }
 }
 
--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -2,7 +2,9 @@
 namespace App\Repository;
 
 use App\Entity\Job;
+use App\Entity\Category;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\AbstractQuery;
 
 class JobRepository extends EntityRepository{
     
@@ -19,6 +21,22 @@ class JobRepository extends EntityRepository{
        ->setParameter('date', new \DateTime())
        ->getQuery()
        ->getOneOrNullResult();
+    }
+
+    /**
+     * @param Category $category
+     * 
+     * @return AbstractQuery
+     */
+    public function getPaginatedActiveJobsByCategoryQuery(Category $category) : AbstractQuery{
+        return $this->createQueryBuilder('j')
+        ->where('j.category = :category')
+        ->andWhere('j.expiresAt > :date')
+        ->andWhere('j.activated = :activated')
+        ->setParameter('category', $category)
+        ->setParameter('date', new \Datetime())
+        ->setParameter('activated', true)
+        ->getQuery();
     }
 
 }
