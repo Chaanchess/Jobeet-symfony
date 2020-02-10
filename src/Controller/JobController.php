d7781bd95ab93448f3cb39e51d1c899e35d01cbb
Chaanchess <kira11fran@gmail.com>
2020-02-10 12:54:51 +0100

    Subida token y edicion token

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Job;
 use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use App\Repository\JobRepository;
 use App\Form\JobType;
+use App\Service\FileUploader;
 
 
 class JobController extends AbstractController{
@@ -57,18 +59,66 @@ class JobController extends AbstractController{
      /**
      * Creates a new job entity.
      *
-     * @Route("/job/create", name="job.create", methods="GET")
+     * @Route("/job/create", name="job.create", methods={"GET", "POST"})
      *
-     * @return Response
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     *
+     * @return RedirectResponse|Response
      */
-    public function create() : Response
+    public function create(Request $request, EntityManagerInterface $em) : Response
     {
         $job = new Job();
         $form = $this->createForm(JobType::class, $job);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile|null $logoFile */
+            $logoFile = $form->get('logo')->getData();
+
+            if ($logoFile instanceof UploadedFile) {
+                $fileName = $fileUploader->upload($logoFile);
+
+                $job->setLogo($fileName);
+            }
+
+            $em->persist($job);
+            $em->flush();
+
+            return $this->redirectToRoute('job.list');
+        }
+        
         return $this->render('job/create.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * Edit existing job entity
+     * 
+     * @Route("/job/{token}/edit", name="job.edit", methods={"GET", "POST"}, requirements={"token" = "\w+"})
+     * 
+     * @param Request $request
+     * @param Job $job
+     * @param EntityManagerInterface $em
+     * 
+     * @return Response
+     */
+    public function edit(Request $request, Job $job, EntityManagerInterface $em) : Response
+    {
+        $form = $this->createForm(JobType::class, $job);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('job.list');
+        }
+        
+        return $this->render('job/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+ 
 }
 ?>
